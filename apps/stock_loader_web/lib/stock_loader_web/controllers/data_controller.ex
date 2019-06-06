661a3d4aae6ed2a94023415ead68163c3a927126
otsybulsky <helg37@gmail.com>
2019-06-06 19:00:11 +0300

    data interval

--- apps/stock_loader_web/lib/stock_loader_web/controllers/data_controller.ex
+++ apps/stock_loader_web/lib/stock_loader_web/controllers/data_controller.ex
@@ -1,9 +1,21 @@
 defmodule StockLoader.Web.DataController do
   use StockLoader.Web, :controller
 
-  def get_m1(conn, %{"sym" => symbol}) do
+  # Valid intervals: [1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo]
+
+  def get_data(conn, %{"sym" => symbol, "interval" => interval}) do
+    range =
+      case interval do
+        "1m" -> "3d"
+        "5m" -> "14d"
+        "1d" -> "3650d"
+        _ -> "14d"
+      end
+
     request =
-      "https://query1.finance.yahoo.com/v8/finance/chart/#{symbol}?symbol=#{symbol}&range=3d&interval=1m&includePrePost=true&events=div%7Csplit%7Cearn&lang=en-SG&region=SG&corsDomain=sg.finance.yahoo.com"
+      "https://query1.finance.yahoo.com/v8/finance/chart/#{symbol}?symbol=#{symbol}&range=#{range}&interval=#{
+        interval
+      }&includePrePost=true&events=div%7Csplit%7Cearn&lang=en-SG&region=SG&corsDomain=sg.finance.yahoo.com"
 
     responce = HTTPoison.get!(request)
 
